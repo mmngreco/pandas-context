683a83499205d5e4c1f647ed5c203e5c57b5e2a9
Maximiliano Greco <mmngreco@gmail.com>
2021-11-09 14:47:09 +0100

    tst: add tests

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -1,2 +1,24 @@
-def test_success():
-    assert True
+import pandas as pd
+import pandas_context  # noqa
+
+
+def test_dataframe_has_context():
+    assert hasattr(pd.DataFrame, "context")
+
+
+def test_series_has_context():
+    assert hasattr(pd.Series, "context")
+
+
+def test_around():
+    s = pd.Series([1, 2, 3, 4, 5], index=[1, 2, 3, 4, 5])
+    obtained = s.context(3, around=2)
+    expected = pd.Series([1, 2, 3, 4, 5], index=[1, 2, 3, 4, 5])
+    pd.testing.assert_series_equal(obtained, expected)
+
+
+def test_pre_post():
+    s = pd.Series([1, 2, 3, 4, 5], index=[1, 2, 3, 4, 5])
+    obtained = s.context(3, pre=2, post=1)
+    expected = pd.Series([1, 2, 3, 4], index=[1, 2, 3, 4])
+    pd.testing.assert_series_equal(obtained, expected)
