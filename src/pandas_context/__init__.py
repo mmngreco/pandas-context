8bf938625d104301a4ca8f1294e3c3a59b46956e
Maximiliano Greco <mmngreco@gmail.com>
2021-11-09 14:54:32 +0100

    doc: add docstring

--- src/pandas_context/__init__.py
+++ src/pandas_context/__init__.py
@@ -8,6 +8,57 @@ del get_versions
 def context(
     pdobj, loc=None, iloc=None, around=2, pre=None, post=None, method="nearest"
 ):
+    """Slicing with a date as reference.
+
+    Parameters
+    ----------
+    loc : index-like
+    iloc : int
+    around : int
+    pre : int
+    post : int
+    method : str
+
+    Returns
+    -------
+    out : pandas-like
+
+    Examples
+    --------
+    >>> import numpy as np
+    ... import pandas_context
+    ... n = 1000
+    ... data = np.random.randn(n)
+    ... index = pd.date_range("19990101", periods=n)
+    ... df = pd.DataFrame(data=data, index=index)
+
+    >>> date = index[50]
+
+    >>> date
+    Timestamp('1999-02-20 00:00:00', freq='D')
+
+    >>> df.context(date)
+                       0
+    1999-02-18  1.086181
+    1999-02-19  0.174156
+    1999-02-20 -0.762305
+    1999-02-21 -0.110677
+    1999-02-22 -0.766751
+
+    >>> df.context(date, around=1)
+                       0
+    1999-02-19  0.174156
+    1999-02-20 -0.762305
+    1999-02-21 -0.110677
+
+    >>> df.context(date, pre=3, post=1)
+                       0
+    1999-02-17 -0.809791
+    1999-02-18  1.086181
+    1999-02-19  0.174156
+    1999-02-20 -0.762305
+    1999-02-21 -0.110677
+    """
 
     if loc is not None:
         idx = pdobj.index.get_loc(loc, method=method)
